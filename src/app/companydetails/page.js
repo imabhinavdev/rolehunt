33421ea2ed461c82e096aacf3d78b37f8fb1b971
Anu <anuskhka@123>
2024-02-16 00:26:50 +0530

    added the user dashboard page

---
+++ src/app/companydetails/page.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import JoblistCard from "@/components/JoblistCard";
+
+const CompanyDetails = () => {
+  return (
+    <div>
+      <div>
+        <P>Profile Name : </P>
+        <p>XYZ </p>
+      </div>
+      <div>
+        <p>Profile Description : </p>
+      </div>
+    </div>
+  );
+};
+
+export default CompanyDetails;
