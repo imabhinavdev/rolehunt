db487722522b6da9c90d77d02b6e2e664149c353
Anu <anuskhka@123>
2024-02-18 01:43:33 +0530

    ADD : added the profile and editprofile page

---
+++ src/app/user/editprofile/page.js
@@ -0,0 +1,119 @@
+import React from "react";
+
+const AddStudent = () => {
+  return (
+    <div>
+      <form>
+        <div className="w-full bg-white rounded-lg shadow border md:mt-0  xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <p className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+              Edit Profile
+            </p>
+            <div className="flex gap-2">
+              <div className="w-1/2">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Email
+                </label>
+                <input
+                  placeholder="JohnDoe"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                  type="text"
+                />
+              </div>
+              <div className="w-1/2">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Enrollment
+                </label>
+                <input
+                  placeholder="email@example.com"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                  type="email"
+                />
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <div className="w-1/2">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Mobile
+                </label>
+                <input
+                  placeholder="9874563210"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                  type="text"
+                />
+              </div>
+              <div className="w-1/2">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Batch
+                </label>
+                <select className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5">
+                  <option value="1">2021</option>
+                  <option value="2">2022</option>
+                  <option value="2">2024</option>
+                  <option value="2">2025</option>
+                </select>
+              </div>
+            </div>
+
+            <div>
+              <p className="text-2xl font-medium mt-10">Update Links </p>
+            </div>
+            <div className="flex gap-2">
+              <div className="w-1/2">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Resume
+                </label>
+                <input
+                  placeholder="https://linkedin.com/in/kishoranushka"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                  type="text"
+                />
+              </div>
+              <div className="w-1/2">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Leetcode
+                </label>
+                <input
+                  placeholder="https://linkedin.com/in/kishoranushka"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                  type="text"
+                />
+              </div>
+            </div>
+            <div className="flex gap-2">
+              <div className="w-1/2">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Linkedin
+                </label>
+                <input
+                  placeholder="https://linkedin.com/in/kishoranushka"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                  type="text"
+                />
+              </div>
+              <div className="w-1/2">
+                <label className="block mb-2 text-sm font-medium text-gray-900">
+                  Github
+                </label>
+                <input
+                  placeholder="https://linkedin.com/in/kishoranushka"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5"
+                  type="text"
+                />
+              </div>
+            </div>
+
+            <button
+              className="w-full bg-yellow hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center  focus:ring-blue-800 text-black"
+              type="submit"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddStudent;
--- src/app/user/profile/page.js
+++ src/app/user/profile/page.js
@@ -1,7 +1,88 @@
-"use client";
-import React, { useEffect } from "react";
+import React from "react";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div className="w-full ">
+      <div class="flex  w-full flex-col shadow-md items-center rounded-md border md:flex-row">
+        <div class="h-full w-full md:h-[200px] md:w-[300px]">
+          <img
+            src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+            alt="Laptop"
+            class="h-full w-full rounded-md object-cover"
+          />
+        </div>
+        <div>
+          <div class="p-5 ">
+            <div className="flex gap-10 items-center justify-between ">
+              <h1 class="inline-flex items-center text-4xl font-semibold">
+                Anushka Kishor
+              </h1>
+
+              <p className="px-2 shadow-md inline-block rounded-full bg-blue text-black   font-medium">
+                Edit Profile
+              </p>
+            </div>
+            <p class="mt-3 text-lg text-gray-600">
+              Developer by profession and Designer by passion
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className="md:flex gap-20 mt-10 ">
+        <div className=" shadow-lg rounded-lg w-full">
+          <div class="flex flex-col p-10 sm:w-full   ">
+            <div class="  ">
+              <div class="grid items-center justify-center w-full grid-cols-1 text-left">
+                <p className="text-3xl  mb-8 font-serif underline text-black">
+                  Details
+                </p>
+                <div className="flex text-black font-medium text-lg gap-10">
+                  <div className="flex-col gap-5 flex">
+                    <p>Email :</p>
+                    <p>Enrollment :</p>
+                    <p>Mobile :</p>
+                    <p>Batch</p>
+                  </div>
+                  <div className="flex-col gap-5 flex text-gray-500">
+                    <p>abc@gmail.com</p>
+                    <p>0187CS211033</p>
+                    <p>7896541230</p>
+                    <p>2025</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className=" w-full  ">
+          <div class="flex flex-col rounded-xl sm:w-full shadow-lg p-10  ">
+            <div class="  ">
+              <div class="grid items-center justify-center w-full grid-cols-1 text-left">
+                <p className="text-3xl  mb-8 font-serif underline text-black">
+                  Links
+                </p>
+                <div className="flex text-black font-medium text-lg gap-10">
+                  <div className="flex-col gap-5 flex">
+                    <p>Resume :</p>
+                    <p>Leetcode :</p>
+                    <p>Linkedin :</p>
+                    <p>Github :</p>
+                  </div>
+                  <div className="flex-col gap-5 flex text-gray-500">
+                    <p>https://linkedin.com/in/kishoranushka</p>
+                    <p>https://linkedin.com/in/kishoranushka</p>
+                    <p>https://linkedin.com/in/kishoranushka</p>
+                    <p>https://linkedin.com/in/kishoranushka</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
